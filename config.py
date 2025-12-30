71873120af5ebe71d8584bee2b047734dfea1a68
Apnimaa <patelkinjal1212@gmail.com>
2025-12-30 10:10:25 +0530

    Update API credentials and owner IDs in config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 
-API_ID = os.environ.get("API_ID", "23664819")
+API_ID = os.environ.get("API_ID", "27433400")
 
-API_HASH = os.environ.get("API_HASH", "3853f97c662d5d08cee5f0d07361361e")
+API_HASH = os.environ.get("API_HASH", "1a286620de5ffe0a7d9b57e604293555")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7694651611:AAFj-XVfUmXbZGK690521MdOlEdg8RygLvs")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8282655063:AAFKE7fkSPMg_nEiaV1gTKY87JK7Jgd-y7s")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-#OWNER = int(os.environ.get("OWNER", 8004315740,7744345481))
+#OWNER = int(os.environ.get("OWNER",6201066540))
 
-#LOG = -1002556158219
+#LOG  =-1003182485007
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
 #try:
-    #ADMINS=[8004315740,7744345481]
-    #for x in (os.environ.get("ADMINS", "8004315740,7744345481").split()):
+    #ADMINS=[6201066540]
+    #for x in (os.environ.get("ADMINS", "6201066540").split()):
         #ADMINS.append(int(x))
 #except ValueError:
         #raise Exception("Your Admins list does not contain valid integers.")
