e0660c0281f6b3b371c7050e9b600e01a58ddefb
rpbotforg-boop <rpbotforg@gmail.com>
2025-12-31 17:36:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 
-API_ID = os.environ.get("API_ID", "27433400")
+API_ID = os.environ.get("API_ID", "22447622")
 
-API_HASH = os.environ.get("API_HASH", "1a286620de5ffe0a7d9b57e604293555")
+API_HASH = os.environ.get("API_HASH", "543b62d58d3e723e766ba57a984ab65d")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8282655063:AAFKE7fkSPMg_nEiaV1gTKY87JK7Jgd-y7s")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7992835160:AAFtC0QuQIBTvIRJKXz2X1pPh4RwSbC7YoA")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-#OWNER = int(os.environ.get("OWNER",6201066540))
+#OWNER = int(os.environ.get("OWNER",777756062))
 
-#LOG  =-1003182485007
+#LOG  =-1002655703820
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
 #try:
-    #ADMINS=[6201066540]
-    #for x in (os.environ.get("ADMINS", "6201066540").split()):
+    #ADMINS=[777756062]
+    #for x in (os.environ.get("ADMINS", "777756062").split()):
         #ADMINS.append(int(x))
 #except ValueError:
         #raise Exception("Your Admins list does not contain valid integers.")
